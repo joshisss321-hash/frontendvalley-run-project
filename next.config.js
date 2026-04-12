16d2da945f7adb2bd296eecd81c47e7dd4de4950
sonujoshi <joshisss321@gmail.com>
2026-04-12 14:50:46 +0530

    ...fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,23 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     domains: ["images.unsplash.com", "images.pexels.com", "res.cloudinary.com"]
+//   }
+// };
+
+// module.exports = nextConfig;
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     domains: ["images.unsplash.com", "images.pexels.com", "res.cloudinary.com"]
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/backend/:path*',
+        destination: 'https://valleyrunproject-production.up.railway.app/:path*',
+      },
+    ]
   }
 };
 
